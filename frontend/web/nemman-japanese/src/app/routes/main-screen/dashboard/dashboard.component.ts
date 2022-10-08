7fedc2a450b26a61a031e11f8b1b13130c9adeed
Lê Đức Huy <leduchuy1461@gmail.com>
2022-10-08 09:34:42 +0700

    Xong luồng dashboard

--- frontend/web/nemman-japanese/src/app/routes/main-screen/dashboard/dashboard.component.ts
+++ frontend/web/nemman-japanese/src/app/routes/main-screen/dashboard/dashboard.component.ts
@@ -28,11 +28,87 @@ export class DashboardComponent implements OnInit {
     {
       rank: 4,
       avatar: 'https://i.postimg.cc/Z5wQKJmh/fchunters.png',
+      name: 'Hoàng Việt Hudwy',
+      word: 10,
+    },
+    {
+      rank: 5,
+      avatar: 'https://i.postimg.cc/Z5wQKJmh/fchunters.png',
       name: 'Hoàng Việt Huy',
       word: 10,
     },
+    {
+      rank: 6,
+      avatar: 'https://i.postimg.cc/Z5wQKJmh/fchunters.png',
+      name: 'Hoàng Việt ưdfw',
+      word: 10,
+    },
+    {
+      rank: 7,
+      avatar: 'https://i.postimg.cc/Z5wQKJmh/fchunters.png',
+      name: 'Hoàng Việt Huy',
+      word: 10,
+    },
+    {
+      rank: 8,
+      avatar: 'https://i.postimg.cc/Z5wQKJmh/fchunters.png',
+      name: 'Hoàng Việt fw',
+      word: 10,
+    },
+    {
+      rank: 8,
+      avatar: 'https://i.postimg.cc/Z5wQKJmh/fchunters.png',
+      name: 'Hoàng Việt fw',
+      word: 10,
+    },
+    {
+      rank: 8,
+      avatar: 'https://i.postimg.cc/Z5wQKJmh/fchunters.png',
+      name: 'Hoàng Việt fw',
+      word: 10,
+    },
+    {
+      rank: 8,
+      avatar: 'https://i.postimg.cc/Z5wQKJmh/fchunters.png',
+      name: 'Hoàng Việt fw',
+      word: 10,
+    },
+    {
+      rank: 8,
+      avatar: 'https://i.postimg.cc/Z5wQKJmh/fchunters.png',
+      name: 'Hoàng Việt fw',
+      word: 10,
+    },
   ];
+
+  listAchievement: any = [
+    {
+      imageUrl: 'https://cdn-icons-png.flaticon.com/512/5455/5455914.png',
+      name: 'Chuỗi 3 ngày',
+      date: '12/09/2022',
+    },
+    {
+      imageUrl: 'https://cdn-icons-png.flaticon.com/512/1835/1835046.png',
+      name: 'Chuỗi 5 ngày',
+      date: '14/09/2022',
+    },
+    {
+      imageUrl: 'https://cdn-icons-png.flaticon.com/512/3021/3021600.png',
+      name: 'Chuỗi 7 ngày',
+      date: '16/09/2022',
+    },
+    {
+      imageUrl: 'https://cdn-icons-png.flaticon.com/512/1834/1834988.png',
+      name: 'Chuỗi 10 ngày',
+      date: '19/09/2022',
+    },
+  ];
+
+  screenHeight: number = 0;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.screenHeight = window.innerHeight;
+  }
 }
