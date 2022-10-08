4317a8f910934a7b01185a43d84e70a690548b31
Tạ Huy Tuấn <94184393+tuanbkite6@users.noreply.github.com>
2022-10-08 13:05:08 +0700

    Creater function courseDetail

--- frontend/web/nemman-japanese/src/app/routes/main-screen/my-wordbooks/my-wordbooks.component.ts
+++ frontend/web/nemman-japanese/src/app/routes/main-screen/my-wordbooks/my-wordbooks.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NzMarks } from 'ng-zorro-antd/slider';
-
+interface courseList {
+  courseName : string ; 
+  courseContent : string ;
+  courseTotal : number ;
+  courseAuthor : string ;
+}
 @Component({
   selector: 'app-my-wordbooks',
   templateUrl: './my-wordbooks.component.html',
@@ -13,33 +18,25 @@ export class MyWordbooksComponent implements OnInit {
   vGutter = 16;
   count = 3;
   array = new Array(this.count);
-  marksHGutter: NzMarks = {
-    8: '8',
-    16: '16',
-    24: '24',
-    32: '32',
-    40: '40',
-    48: '48'
-  };
-  marksVGutter: NzMarks = {
-    8: '8',
-    16: '16',
-    24: '24',
-    32: '32',
-    40: '40',
-    48: '48'
-  };
-  marksCount: NzMarks = {
-    2: '2',
-    3: '3',
-    4: '4',
-    6: '6',
-    8: '8',
-    12: '12'
-  };
-  reGenerateArray(count: number): void {
-    this.array = new Array(count);
-  }
+  userCourseDetail : courseList[] = [{
+    courseName : 'Minna ni hongo' ,
+    courseContent : "すみません" ,
+    courseTotal : 20  , 
+    courseAuthor : 'Huy Tuấn' 
+  },
+  {
+    courseName : 'Minna ni hongo' ,
+    courseContent : "すみません" ,
+    courseTotal : 20  , 
+    courseAuthor : 'Huy Tuấn' 
+  },
+  {
+    courseName : 'Minna ni hongo' ,
+    courseContent : "すみません" ,
+    courseTotal : 20  , 
+    courseAuthor : 'Huy Tuấn' 
+  },
+]
   ngOnInit(): void {
   }
 
