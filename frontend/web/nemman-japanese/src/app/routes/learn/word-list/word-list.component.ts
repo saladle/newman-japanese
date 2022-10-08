a33332f20c33e01e66531f93464b878701b7e678
Lê Đức Huy <leduchuy1461@gmail.com>
2022-10-08 13:06:25 +0700

    word list, word detail screen

--- frontend/web/nemman-japanese/src/app/routes/learn/word-list/word-list.component.ts
+++ frontend/web/nemman-japanese/src/app/routes/learn/word-list/word-list.component.ts
@@ -3,13 +3,103 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-word-list',
   templateUrl: './word-list.component.html',
-  styleUrls: ['./word-list.component.css']
+  styleUrls: ['./word-list.component.css'],
 })
 export class WordListComponent implements OnInit {
+  listOfData: any = [
+    {
+      key: '1',
+      name: '皆さんこんにちは',
+      age: 'xin chào mọi người',
+      address: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'こんばんは',
+      age: 'chào buổi tối',
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
