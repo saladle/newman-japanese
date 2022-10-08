6286f7096c67daffaabb20cdbff5160061c0fb0a
VietDung7301 <vietdung.7301@gmail.com>
2022-10-08 08:39:04 +0700

    init backend

---
+++ backend/JLearning/JLearning.api/Consts.cs
@@ -0,0 +1,7 @@
+namespace JLearning.api
+{
+    public static class consts
+    {
+        public const string dbConnection = "Server=localhost;Port=3309;Database=learning;Uid=root;Password=#nothingspecial";
+    }
+}
