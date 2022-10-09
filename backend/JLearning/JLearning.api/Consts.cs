3bc9f2cfecd2215377316519c445c3eb8bcbda19
Lê Đức Huy <leduchuy1461@gmail.com>
2022-10-09 09:59:07 +0700

    remove unsued backend

--- backend/JLearning/JLearning.api/Consts.cs
+++
@@ -1,7 +0,0 @@
-namespace JLearning.api
-{
-    public static class consts
-    {
-        public const string dbConnection = "Server=localhost;Port=3309;Database=learning;Uid=root;Password=#nothingspecial";
-    }
-}
