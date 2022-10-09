27ba267cc5567265dbc45879cf0026cb3b6932b1
Lê Đức Huy <leduchuy1461@gmail.com>
2022-10-09 09:53:24 +0700

    many screen

--- frontend/web/nemman-japanese/src/app/routes/main-screen/analysis/analysis.component.ts
+++ frontend/web/nemman-japanese/src/app/routes/main-screen/analysis/analysis.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-analysis',
   templateUrl: './analysis.component.html',
-  styleUrls: ['./analysis.component.css']
+  styleUrls: ['./analysis.component.css'],
 })
 export class AnalysisComponent implements OnInit {
+  listAchievement: any = [
+    {
+      imageUrl: 'https://cdn-icons-png.flaticon.com/512/5455/5455914.png',
+      name: 'Chuỗi 3 ngày',
+      date: '12/09/2022',
+    },
+    {
+      imageUrl: 'https://cdn-icons-png.flaticon.com/512/1835/1835046.png',
+      name: 'Chuỗi 5 ngày',
+      date: '14/09/2022',
+    },
+    {
+      imageUrl: 'https://cdn-icons-png.flaticon.com/512/3021/3021600.png',
+      name: 'Chuỗi 7 ngày',
+      date: '16/09/2022',
+    },
+    {
+      imageUrl: 'https://cdn-icons-png.flaticon.com/512/1834/1834988.png',
+      name: 'Chuỗi 10 ngày',
+      date: '19/09/2022',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- frontend/web/nemman-japanese/src/app/routes/main-screen/created-wordbooks/created-wordbooks.component.ts
+++ frontend/web/nemman-japanese/src/app/routes/main-screen/created-wordbooks/created-wordbooks.component.ts
@@ -1,15 +1,49 @@
+import { WordbookService } from './../../../services/wordbook-management/wordbook.service';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { staticPath } from 'src/app/utils/staticPath';
 
 @Component({
   selector: 'app-created-wordbooks',
   templateUrl: './created-wordbooks.component.html',
-  styleUrls: ['./created-wordbooks.component.css']
+  styleUrls: ['./created-wordbooks.component.css'],
 })
 export class CreatedWordbooksComponent implements OnInit {
+  allWordBook: any = [];
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private wordbookService: WordbookService
+  ) {}
+  hGutter = 16;
+  vGutter = 16;
+  count = 3;
+  array = new Array(this.count);
 
   ngOnInit(): void {
+    this.fetchData();
   }
 
+  async fetchData() {
+    await this.getAllWordBook();
+  }
+
+  async getAllWordBook() {
+    var response = await this.wordbookService.getAllWordbook().toPromise();
+    this.allWordBook = response;
+    console.log('this.allMatches');
+    console.log(this.allWordBook);
+    console.log(this.allWordBook[0]);
+  }
+
+  goToDetailWord(wordBook: any): any {
+    var id = wordBook.id;
+    this.router.navigate([`/${staticPath.WORD_LIST}`, { id }]);
+    this.wordbookService.setCurrentWordBook(wordBook);
+    // this.saveDataCache(constant.CACHE_WORDBOOK_LABEL, wordBook);
+  }
+
+  saveDataCache(label: string, data: any) {
+    localStorage.setItem(label, JSON.stringify(data));
+  }
 }
